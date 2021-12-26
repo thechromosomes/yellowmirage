0517abf848c0e85b5bdc199b6904bcc632e2f864
sagar_gh <sagarkumar01.gh@gmail.com>
2021-12-26 16:49:39 +0530

    target static in config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,18 +25,8 @@ export default {
     "~/static/assets/vendors/jquery-flipster/css/jquery.flipster.css",
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  target: "static", // default is 'server'
 };
